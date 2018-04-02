de1a6efea6ed9f9dc47f8d2d5bb81740c1ca8822
Kei Fukutani <kifinewera@gmail.com>
2018-04-02 17:07:26 +0900

    added con-dims.py

---
+++ practice_projects/CNNs/conv-dims.py
@@ -0,0 +1,13 @@
+# CNN. Dimensionality. 
+# Author : Udacity + kei
+# Date   : April 2, 2018
+# Note   : Python 2.7 required. 
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding='valid', 
+    activation='relu', input_shape=(200, 200, 1)))
+model.summary()
